b5b3021b4dd6cd610c1f7b32209bc07abf458d32
blupblupdoux <sarah.melin19403@gmail.com>
2020-11-03 15:49:25 +0100

    add nelmio cors bundle

---
+++ vuejs_app/src/views/Test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="test">
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos dolore nisi nobis, doloribus a nihil minus amet obcaecati enim commodi nostrum similique, alias in sed maxime. Laudantium, odio at! Rerum.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Test',
+}
+</script>
