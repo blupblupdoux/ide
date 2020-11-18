e267ae2d876c8852e988d350590658a090b648a5
blupblupdoux <sarah.melin19403@gmail.com>
2020-11-18 15:18:57 +0100

    form front add and edit + form add back

--- symfony_app/src/Controller/UserController.php
+++ symfony_app/src/Controller/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -25,6 +27,7 @@ class UserController extends AbstractController
      * @var SerializerInterface
      */
     private $serializer;
+    
 
     public function __construct(UserRepository $userRepository, SerializerInterface $serializer)
     {
@@ -50,5 +53,34 @@ class UserController extends AbstractController
         $user = $this->userRepository->find($id);
         $user_array = $this->serializer->normalize($user, null, ['groups' => ['user']]);
         return $this->json($user_array);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $user = new User();
+        
+        $form = $this->createForm(UserType::class, $user);
+        $form->submit($data);
+
+        if($form->isSubmitted()) {
+
+            $user = $form->getData();
+
+            $user->setRoles(["ROLE_ADMIN"]);
+
+            $user->setPassword('$argon2id$v=19$m=65536,t=4,p=1$1p0D/j/2PrQKvfxAz/J+SA$NYaUQx44eRoyWtHcww4TjndGeYrqq26aYLZA7tdLMvQ');
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->json($user, 201);
+        } else {
+            return $this->json($user, 500);
+        }
     }
 }
